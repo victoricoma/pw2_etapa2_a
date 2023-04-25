f956708f944a65a4c12dd9b141901a7f2b56b38d
victoricoma <victorsena3010@gmail.com>
2023-04-24 22:18:04 -0300

    Aula2404 Parte 1 middleware

--- appExpress/index.js
+++ appExpress/index.js
@@ -5,6 +5,18 @@ const path = require('path')
 
 const basePath = path.join(__dirname,'templates')
 
+var checkAuth = function(req, res, next){
+    req.authStatus = false
+    if(req.authStatus){
+        console.log('Está Logado pode continuar!')
+        next()
+    }else{
+        console.log('Não esta logado, faça login para continuar!')
+    }
+}
+
+app.use(checkAuth)
+
 app.get('/', (req, res)=>{
     res.sendFile(`${basePath}/index.html`)
 })
