21fed55016469c373ce045bab7a8f7b1ed310c3d
victoricoma <victorsena3010@gmail.com>
2023-05-08 22:44:31 -0300

    Aula0805 - Pagina 404 e Gestão de Arquivos no Servidor

--- appExpress/index.js
+++ appExpress/index.js
@@ -22,6 +22,12 @@ app.get('/', (req, res)=>{
     res.sendFile(`${basePath}/index.html`)
 })
 
+app.use(
+    function(req, res, next){
+        res.status(404).sendFile(`${basePath}/404.html`)
+    }
+)
+
 app.listen(port, () => {
     console.log(`Servidor Rodando na porta: ${port}`)
 })
--- appExpress/user/index.js
+++ appExpress/user/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const fs = require('fs')
 const basePath = path.join(__dirname,'../templates')
 
 router.get('/add', (req, res) => {
@@ -11,8 +12,25 @@ router.post('/save', (req, res) =>{
     let name = req.body.name
     let age = req.body.age
 
-    console.log(`Nome do Usuario: ${name}, Idade: ${age}`)
-    res.sendFile(`${basePath}/userForm.html`)
+    if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+    }
+
+    if (fs.existsSync(`accounts/${name}.json`)) {
+        res.status(409).sendFile(`${basePath}/erro.html`)
+        return
+    }
+
+    fs.writeFileSync(
+        `accounts/${name}.json`,
+        `{"name":"${name}", "age":${age}}`,
+        function (err) {
+            console.error(err)
+        }
+    )
+
+    console.log(`Nome do Usuario: ${name}, Idade: ${age} | Estão salvos no servidor.`)
+    res.status(201).sendFile(`${basePath}/userForm.html`)
 })
 
 module.exports = router
