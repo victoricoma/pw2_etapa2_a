5cf9fa1ba3fa8df26f6d826b5b453c640134f213
victoricoma <victorsena3010@gmail.com>
2023-05-15 22:45:46 -0300

    Aula1505 Métodos Handlebars

--- appHandlebars/index.js
+++ appHandlebars/index.js
@@ -6,6 +6,20 @@ const app = express()
 
 app.engine('handlebars', exphbs())
 app.set('view engine','handlebars')
+const approved =false;
+
+app.post('/verifyUser', (req, res) =>{
+    const form ={
+        nome:req.body.name,
+        idade:req.body.age
+    }
+    if(form.nome=='Gisley' && form.age==52){
+        approved=true
+    }else{
+        approved=false
+    }
+    res.render('home', {user:user, auth:approved, approved})
+})
 
 app.get('/', function(req, res){
     const user ={
@@ -13,7 +27,9 @@ app.get('/', function(req, res){
         surname: "Icoma",
         age: 41
     }
-    res.render('home', {user:user})
+    res.render('home', {user:user, auth:approved, approved})
 })
-
+app.get('/dashboards', function(req, res){
+    res.render('dashboards')
+})
 app.listen(3000)
