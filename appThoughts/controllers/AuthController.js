12cad45eca8201f8d18f18805d0ebf2a850512c5
victoricoma <victorsena3010@gmail.com>
2023-06-05 22:39:53 -0300

    Aula0506 Rotas e Controles de aplicação

--- appThoughts/controllers/AuthController.js
+++ appThoughts/controllers/AuthController.js
@@ -8,6 +8,37 @@ module.exports = class UserController {
 
     static async loginPost(req, res){
         const {email, password} = req.body
+
+        const user = await User.findOne({ where: {email: email}})
+
+        if(!user){
+            res.render('auth/login',{
+                message: 'Usuário não encontrado'
+            })
+
+            return
+        }
+
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if(!passwordMatch){
+            res.render('auth/login',{
+                message: 'Senha inválida'
+            })
+
+            return
+        }
+
+        req.session.userid = user.id
+        req.flash('message', 'Login realizado com sucesso')
+
+        req.session.save(() => {
+            res.redirect('/')
+        })
+    }
+
+    static async register(req, res){
+        res.render('auth/register')
     }
 
     static async registerPost(req, res){
@@ -28,5 +59,30 @@ module.exports = class UserController {
 
             return
         }
+        // Volume de bits enbaralhados e técnica de criptografia
+        const salt = bcrypt.genSaltSync(10)
+        // Embaralha baseado na linha anterior
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        User.create(user)
+        .then((user) =>{
+            req.session.userid = userid
+            req.flash('message','Cadastro executado com sucesso!')
+            req.session.save(()=>{
+                res.redirect('/')
+            })
+        })
+        .catch((err) => console.error(err))
+    }
+
+    static logout(req, res){
+        req.session.destroy()
+        res.redirect('/login')
     }
 }
